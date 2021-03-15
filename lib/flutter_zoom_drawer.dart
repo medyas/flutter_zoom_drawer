47f88aab89a339116d5f36ed5c63fc91ca735e4f
nguyenvantoan <toannv@rnlab.io>
2021-03-15 17:44:47 +0700

    fix scaleRight

--- lib/flutter_zoom_drawer.dart
+++ lib/flutter_zoom_drawer.dart
@@ -405,8 +405,7 @@ class _ZoomDrawerState extends State<ZoomDrawer> with SingleTickerProviderStateM
   }
 
   Widget renderScaleRight() {
-    final slidePercent = widget.isRTL ? MediaQuery.of(context).size.width * .08 : 15.0;
-
+    final slidePercent = widget.isRTL ? MediaQuery.of(context).size.width * .085 : 15.0;
     return AnimatedBuilder(
       animation: _animationController!,
       builder: (context, child) {
